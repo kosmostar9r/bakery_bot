3e7be0a83aab2e4b7b5c78fe7253c3a1f655b4fd
LeksaKonovalov <kosmostar9r@gmail.com>
2021-05-29 23:42:03 +0300

    useless part deleted

--- database_updater.py
+++ database_updater.py
@@ -10,14 +10,8 @@ def format_img_binary(img_path):
 def update_categories(cats, prods):
     for product in elements_by_category:
         product_category, created = cats.get_or_create(category=product['category'])
-        if not created:
-            cats.update(category=product['category']).where(cats.category == product_category.category).execute()
         binary_img = format_img_binary(product['image'])
-        prod, created = prods.get_or_create(title=product['title'],
-                                            description=product['description'],
-                                            price=product['price'],
-                                            photo=binary_img,
-                                            category=product_category)
+        prod, created = prods.get_or_create(title=product['title'])
         if not created:
             prods.update(title=product['title'],
                          description=product['description'],
