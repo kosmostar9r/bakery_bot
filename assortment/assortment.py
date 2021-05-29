a71a42f8dfcbfffa1ee54e5a76b3332f188aad37
LeksaKonovalov <kosmostar9r@gmail.com>
2021-05-29 16:27:42 +0300

    this module is used to create data for db

--- assortment/assortment.py
+++ assortment/assortment.py
@@ -1,85 +1,86 @@
-elements_by_category = {
-    'Круассаны': [
+elements_by_category = [
         {
             'title': 'Миндальный круассан',
             'description': 'Цена: 250р',
-            'image': 'assortment/images/croissants/almond_croissant.jpg'
+            'image': 'assortment/images/croissants/almond_croissant.jpg',
+            'price': 250,
+            'category': 'Круассаны'
         },
         {
             'title': 'Классический круассан',
             'description': 'Цена: 150р',
-            'image': 'assortment/images/croissants/classic_croissant.jpg'
+            'image': 'assortment/images/croissants/classic_croissant.jpg',
+            'price': 150,
+            'category': 'Круассаны'
         },
         {
             'title': 'Круассан с малиной',
             'description': 'Цена: 250р',
-            'image': 'assortment/images/croissants/raspberry_croissant.jpg'
+            'image': 'assortment/images/croissants/raspberry_croissant.jpg',
+            'price': 250,
+            'category': 'Круассаны'
         },
-    ],
-    'Эклеры': [
         {
             'title': 'Шоколадный эклер',
             'description': 'Цена: 220р',
-            'image': 'assortment/images/eclairs/chocolate_eclair.jpg'
+            'image': 'assortment/images/eclairs/chocolate_eclair.jpg',
+            'price': 220,
+            'category': 'Эклеры'
         },
         {
             'title': 'Эклер малина-личи',
             'description': 'Цена: 240р',
-            'image': 'assortment/images/eclairs/raspberry_lichi_eclair.jpg'
+            'image': 'assortment/images/eclairs/raspberry_lichi_eclair.jpg',
+            'price': 240,
+            'category': 'Эклеры'
         },
         {
             'title': 'Фисташковый эклер',
             'description': 'Цена: 240р',
-            'image': 'assortment/images/eclairs/pistachio_eclair.jpg'
+            'image': 'assortment/images/eclairs/pistachio_eclair.jpg',
+            'price': 240,
+            'category': 'Эклеры'
         },
-    ],
-    'Торты': [
         {
             'title': 'Медовик',
             'description': 'Цена: 280р',
-            'image': 'assortment/images/cakes/honey_cake.jpg'
+            'image': 'assortment/images/cakes/honey_cake.jpg',
+            'price': 280,
+            'category': 'Торты'
         },
         {
             'title': 'Лимонный тарт',
             'description': 'Цена: 280р',
-            'image': 'assortment/images/cakes/lemon_tart.jpg'
+            'image': 'assortment/images/cakes/lemon_tart.jpg',
+            'price': 280,
+            'category': 'Торты'
         },
         {
             'title': 'Мусс красный куб',
             'description': 'Цена: 320р',
-            'image': 'assortment/images/cakes/red_cube_cake.jpg'
+            'image': 'assortment/images/cakes/red_cube_cake.jpg',
+            'price': 320,
+            'category': 'Торты'
         },
-    ],
-    'Закуски': [
         {
             'title': 'Киш с ветчиной',
             'description': 'Цена: 350р',
-            'image': 'assortment/images/snacks/pork_cheese_pie.jpg'
+            'image': 'assortment/images/snacks/pork_cheese_pie.jpg',
+            'price': 350,
+            'category': 'Закуски'
         },
         {
             'title': 'Сэндвич с тунцом',
             'description': 'Цена: 370р',
-            'image': 'assortment/images/snacks/tuna_sandwich.jpg'
+            'image': 'assortment/images/snacks/tuna_sandwich.jpg',
+            'price': 370,
+            'category': 'Закуски'
         },
         {
             'title': 'Сэндвич с лососем',
             'description': 'Цена: 450р',
-            'image': 'assortment/images/snacks/salmon_sandwich.jpg'
+            'image': 'assortment/images/snacks/salmon_sandwich.jpg',
+            'price': 450,
+            'category': 'Закуски'
         },
-    ],
-}
-
-prices = {
-    'Миндальный круассан': 250,
-    'Классический круассан': 150,
-    'Круассан с малиной': 250,
-    'Шоколадный эклер': 220,
-    'Эклер малина-личи': 240,
-    'Фисташковый эклер': 240,
-    'Медовик': 280,
-    'Лимонный тарт': 280,
-    'Мусс красный куб': 320,
-    'Киш с ветчиной': 350,
-    'Сэндвич с тунцом': 370,
-    'Сэндвич с лососем': 450
-}
+]
