e3cc413cd973d79238cab9d4af17083da7a74060
LeksaKonovalov <kosmostar9r@gmail.com>
2021-05-29 23:43:01 +0300

    method get_photo() converted into single string code

--- templates.py
+++ templates.py
@@ -2,20 +2,13 @@ from io import BytesIO
 from models import Product, Categories
 
 
-def get_photo(image):
-    temp_file = BytesIO(image)
-    temp_file.seek(0)
-    return temp_file
-
-
 def create_elements(category):
     elements = []
     category = Categories.get(category=category)
     products = [cat for cat in Product.select().where(Product.category == category)]
     for product in products:
-        photo = get_photo(product.photo)
         template = {
-            'photo_id': photo,
+            'photo_id': BytesIO(product.photo),
             'title': product.title,
             'description': product.description,
             'action': {
